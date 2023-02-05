d54b67941eff9c17945c2246eb80bc10b826841f
Skrigan <sk_dm-821@mail.ru>
2023-02-05 22:14:04 +0300

    add: clientId

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,13 +1,26 @@
 const chat = document.querySelector(".chat");
-const ws = new WebSocket("wss://rs-clone-api-production.up.railway.app");
-// const ws = new WebSocket("wss://127.0.0.1:3000");
+// const ws = new WebSocket("wss://rs-clone-api-production.up.railway.app");
+const ws = new WebSocket("ws://127.0.0.1:3000");
 ws.onopen = () => {
   console.log(JSON.stringify("connected!"));
 };
-ws.onmessage = (message) => {
-  const messages = JSON.parse(message.data) as Array<{name: string, message: string}>;
 
-  messages.forEach(el => {
+let clientId = null;
+
+type Message = {
+  method: string;
+  clientId: string;
+  messages: Array<{name: string, message: string}>
+}
+ws.onmessage = (message) => {
+  // const messages = JSON.parse(message.data) as Array<{name: string, message: string}>;
+  const responce = JSON.parse(message.data) as Message;
+  if (responce.method === "connect") {
+    clientId = responce.clientId;
+  }
+
+  // messages.forEach(el => {
+  responce.messages.forEach(el => {
     const messageEl = document.createElement("div");
     messageEl.innerText = `${el.name}: ${el.message}`;
     chat?.appendChild(messageEl);
