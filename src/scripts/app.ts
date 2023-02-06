08ebf407a8e0a13f30baac330e536d38cc066352
Skrigan <sk_dm-821@mail.ru>
2023-02-06 04:02:51 +0300

    add link

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,30 +1,87 @@
-const chat = document.querySelector(".chat");
-// const ws = new WebSocket("wss://rs-clone-api-production.up.railway.app");
-const ws = new WebSocket("ws://127.0.0.1:3000");
-ws.onopen = () => {
-  console.log(JSON.stringify("connected!"));
-};
-
-let clientId = null;
-
 type Message = {
   method: string;
   clientId: string;
-  messages: Array<{name: string, message: string}>
+  messages: Array<{ name: string, message: string }>
 }
+
+// const ws = new WebSocket("wss://rs-clone-api-production.up.railway.app");
+const ws = new WebSocket("ws://127.0.0.1:3000");
+
+const chat = document.querySelector(".chat")!;
+const btnCreate = document.querySelector("#btnCreate")!;
+const btnJoin = document.querySelector("#btnJoin")!;
+const txtGameId = document.querySelector("#txtGameId") as HTMLInputElement;
+const divPlayers = document.querySelector("#divPlayers")!;
+
+
+
+let clientId: string;
+let gameId: string;
+
+// const chatCont = document.querySelector(".chat-cont")?.classList.toggle("hidden");
+// const changeChatVisability = () {}
+
+
+//CREATE GAME
+btnCreate?.addEventListener("click", (e) => {
+  const payLoad = {
+    "method": "create",
+    "clientId": clientId,
+  }
+  ws.send(JSON.stringify(payLoad));
+})
+btnJoin?.addEventListener("click", (e) => {
+  if (!gameId) gameId = txtGameId.value;
+  const payLoad = {
+    "method": "join",
+    "clientId": clientId,
+    "gameId": gameId,
+  }
+  ws.send(JSON.stringify(payLoad));
+
+})
+
 ws.onmessage = (message) => {
   // const messages = JSON.parse(message.data) as Array<{name: string, message: string}>;
-  const responce = JSON.parse(message.data) as Message;
-  if (responce.method === "connect") {
-    clientId = responce.clientId;
-    console.log('clientId: ', clientId)
+  const responce = JSON.parse(message.data) as any;
+  switch (responce.method) {
+    case "connect": {
+      clientId = responce.clientId;
+      console.log("clientId: ", clientId);
+      break;
+    }
+    case "create": {
+      gameId = responce.game.id;
+      console.log("gameId: ", gameId);
+      break;
+    }
+    case "join": {
+      const game = responce.game;
+
+      while (divPlayers?.firstChild) divPlayers?.removeChild(divPlayers.firstChild!);
+
+      game.clients.forEach((c: any) => {
+        const d = document.createElement("div");
+        d.style.width = "200px";
+        d.style.background = c.color;
+        d.textContent = c.clientId;
+        divPlayers.appendChild(d);
+      })
+      // console.log("gameId: ", gameId);
+      break;
+    }
   }
+  // if (responce.method === "connect") {
+  //   clientId = responce.clientId;
+  //   console.log("clientId: ", clientId)
+  // }
+  // if ()
   // messages.forEach(el => {
-  responce.messages.forEach(el => {
-    const messageEl = document.createElement("div");
-    messageEl.innerText = `${el.name}: ${el.message}`;
-    chat?.appendChild(messageEl);
-  });
+  // responce.messages.forEach(el => {
+  //   const messageEl = document.createElement("div");
+  //   messageEl.innerText = `${el.name}: ${el.message}`;
+  //   chat?.appendChild(messageEl);
+  // });
 };
 
 const send = (event: Event) => {
@@ -36,7 +93,7 @@ const send = (event: Event) => {
   const messageInputEl = document.getElementById("message") as HTMLInputElement;
   const message = messageInputEl.value;
 
-  ws.send(JSON.stringify({name, message}));
+  ws.send(JSON.stringify({ name, message }));
 
 };
 
@@ -51,4 +108,4 @@ sendBtn?.addEventListener("click", send);
 
 
 
- 
+
