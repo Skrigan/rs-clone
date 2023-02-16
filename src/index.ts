153f0813fddf456a1f298d5d2a52a27effbe75a1
Sasha <hlupik69@gmail.com>
2023-02-16 20:46:19 +0300

    feat: add color theme

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import "./styles/chat.scss";
 import "./styles/registration.scss";
 import "./styles/modificators.scss";
 import "./styles/header.scss";
-import "./styles/localozation.scss"
+import "./styles/localozation.scss";
+import "./styles/game.scss";
 import "./scripts/app";
 import "./scripts/mainPage";
 import "./scripts/router";
-import "./scripts/localization"
+import "./scripts/localization";
